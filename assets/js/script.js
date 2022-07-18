d7875a9bb733685167e2e5ce84f973990330e7e1
HoneyTwix <chocolateenchanted@gmail.com>
2022-07-18 03:28:28 -0500

    js

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+// Show date
+var todayDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(todayDate);
+
+$(document).ready(function () {
+    // saveBtn function
+    $(".saveBtn").on("click", function () {
+        // Get nearby values of the description in JQuery
+        var text = $(this).siblings(".task").val();
+        var time = $(this).parent().attr("id");
+
+        // Save local storage
+        localStorage.setItem(time, text);
+    })
+   
+    function timeTracker() {
+        //get hours.
+        var timeNow = moment().hour();
+
+        // loop over time blocks
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            // To check the time and add the classes for background indicators
+            if (blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+
+            }
+        })
+    }
+
+    // Get item from local storage if any
+    $("#hour8 .task").val(localStorage.getItem("hour8"));
+    $("#hour9 .task").val(localStorage.getItem("hour9"));
+    $("#hour10 .task").val(localStorage.getItem("hour10"));
+    $("#hour11 .task").val(localStorage.getItem("hour11"));
+    $("#hour12 .task").val(localStorage.getItem("hour12"));
+    $("#hour13 .task").val(localStorage.getItem("hour13"));
+    $("#hour14 .task").val(localStorage.getItem("hour14"));
+    $("#hour15 .task").val(localStorage.getItem("hour15"));
+    $("#hour16 .task").val(localStorage.getItem("hour16"));
+    $("#hour17 .task").val(localStorage.getItem("hour17"));
+
+    timeTracker();
+})
+   
